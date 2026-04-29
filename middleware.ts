b98b5e1eb29b57f9f8d93e61a00cc8bee2e30506
zaidikamal <zaidikamal@github.com>
2026-04-29 20:27:12 +0100

    fix(middleware): resolve session drop bug during token refresh

--- middleware.ts
+++ middleware.ts
@@ -27,16 +27,10 @@ export async function middleware(request: NextRequest) {
       },
       set(name: string, value: string, options: CookieOptions) {
         request.cookies.set({ name, value, ...options });
-        response = NextResponse.next({
-          request: { headers: request.headers },
-        });
         response.cookies.set({ name, value, ...options });
       },
       remove(name: string, options: CookieOptions) {
         request.cookies.set({ name, value: "", ...options });
-        response = NextResponse.next({
-          request: { headers: request.headers },
-        });
         response.cookies.set({ name, value: "", ...options });
       },
     },
